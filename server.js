0fe3a5e0e23d23ec4df63028b77da70730592038
Hugo Scelers <hugo.scelers@outlook.com>
2023-05-12 15:00:12 +0200

    modif cors

--- server.js
+++ server.js
@@ -25,10 +25,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json())
 
-app.use('/api/v1', require('./routes/v1'))
-
 const cors = require('cors')
 app.use(cors())
+
+app.use('/api/v1', require('./routes/v1'))
+
+
 
 app.get('*', (req, res) => {
     // res.status(404).json({message: 'Not found'}) bonne pratique
